6c16a314911fe12851bc1fc684f8a728cf7fe77b
Joselian2204 <josemar.elian.castro.paz@gmail.com>
2021-10-25 18:41:09 -0400

    Ascensor y AscnesorTest creados

---
+++ src/test/java/AscensorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class AscensorTest {
+
+}
