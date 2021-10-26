242bbfab532a65eb698caf31e40308c61ac0298e
Joselian2204 <josemar.elian.castro.paz@gmail.com>
2021-10-25 20:05:47 -0400

    funcionalidad

--- src/main/java/Ascensor.java
+++ src/main/java/Ascensor.java
@@ -1,2 +1,63 @@
 public class Ascensor {
+    private int pisoActual = 0;
+    private int pisoDestino;
+    private int pisoPersona = -1;
+    private boolean primerviaje = false;
+
+    public int ocupado(int pp, int pa){
+        if(pp == pa){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public boolean crearPersona(){
+        pisoPersona = (int) (Math.random()*2);
+        if(pisoPersona >= 0 && pisoPersona < 3){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int llamarPiso(int pl){
+        if(pl != pisoActual){
+            pisoDestino = pl;
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public boolean primerViaje(){
+        if ()
+    }
+
+    public int getPisoActual() {
+        return pisoActual;
+    }
+
+    public void setPisoActual(int pisoActual) {
+        this.pisoActual = pisoActual;
+    }
+
+    public int getPisoDestino() {
+        return pisoDestino;
+    }
+
+    public void setPisoDestino(int pisoDestino) {
+        this.pisoDestino = pisoDestino;
+    }
+
+    public int getPisoPersona() {
+        return pisoPersona;
+    }
+
+    public void setPisoPersona(int pisoPersona) {
+        this.pisoPersona = pisoPersona;
+    }
 }
--- src/test/java/AscensorTest.java
+++ src/test/java/AscensorTest.java
@@ -8,4 +8,44 @@ import static org.junit.Assert.*;
 
 public class AscensorTest {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private Ascensor ascensor;
+
+    @Before
+    public final void before(){
+        //1. Preparacin de la prueba
+        ascensor = new Ascensor();
+        System.out.println("Inicializando Prueba..." + ascensor.toString());
+    }
+
+    @Test
+    public void ascensorListo(){
+        //3. Verificacion o Assert
+        assertEquals(1,ascensor.ocupado(1,1));
+    }
+
+    @Test
+    public void existePersona(){
+        assertEquals(true,ascensor.crearPersona());
+    }
+
+    @Test
+    public void noExistePersona(){
+        assertEquals(-1, ascensor.getPisoPersona());
+    }
+
+    @Test
+    public void pisoDestinoDiferentePisoPersona(){
+        ascensor.setPisoActual(2);
+        assertEquals(1,ascensor.llamarPiso(1));
+    }
+
+    @Test
+    public void primerViajePersona(){
+        assertEquals(true,ascensor.primerViaje());
+    }
+
+
 }
